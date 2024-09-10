56aaea1bdbb78270aed88c6c24c3f47429148c9c
quebim <kevinledesmam95@gmail.com>
2024-09-09 22:22:35 -0300

    style: Improve RestTaskGetActionTests readablity and styling

--- src/test/java/org/opensearch/tasks/action/RestTaskGetActionTests.java
+++ src/test/java/org/opensearch/tasks/action/RestTaskGetActionTests.java
@@ -7,20 +7,33 @@
  */
 package org.opensearch.tasks.action;
 
+import org.junit.Before;
 import org.opensearch.action.get.GetRequest;
+import org.opensearch.tasks.handler.RestTaskHandler;
 import org.opensearch.tasks.model.Task;
 import org.opensearch.test.OpenSearchTestCase;
 import org.opensearch.test.rest.FakeRestRequest;
 
 import java.util.Collections;
+import java.util.Locale;
 
 public class RestTaskGetActionTests extends OpenSearchTestCase {
+    private String path;
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        this.path =  String.format(Locale.ROOT, "%s", RestTaskHandler.BASE_URI);
+    }
 
     public void testGetRequest() {
         String taskId = "task_123";
-        FakeRestRequest request = new FakeRestRequest.Builder(xContentRegistry())
-                .withParams(Collections.singletonMap("id", taskId))
-                .build();
+
+        FakeRestRequest.Builder builder = new FakeRestRequest.Builder(xContentRegistry());
+        builder.withPath(this.path + "/" + taskId);
+        builder.withParams(Collections.singletonMap("id", taskId));
+
+        FakeRestRequest request = builder.build();
 
         GetRequest getRequest = RestTaskGetAction.getRequest(request);
 
